39cab6c282aab931d2bc426ba2868909b5b30e55
Elise La Rooy <eliselarooy@gmail.com>
2022-03-23 18:48:22 +0000

    added runServer function

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
 import express from 'express';
+import { port } from './config/environment.js';
+import { connectToDB } from './db/helpers.js';
+import router from './config/router.js';
+import mongoSanitize from 'express-mongo-sanitize';
+
+const app = express();
+app.use(express.json());
+
+app.use(mongoSanitize());
+
+app.use('/api', router);
+
+async function runServer() {
+  console.log('Connecting to db');
+  await connectToDB();
+  console.log('Connected to db');
+  app.listen(port, () => console.log(`App listening on port: ${port}`));
+}
+
+runServer();
