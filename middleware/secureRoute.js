4e7f433b9835fe1a285777e0a93de19abb0d3e57
Ashley Gyngell <ashleygyngell@gmail.com>
2022-03-24 14:44:31 +0000

    secureRoute added -  just need to do a npm i jsonwebtoken

---
+++ middleware/secureRoute.js
@@ -0,0 +1,37 @@
+// import jwt from 'jsonwebtoken';
+import User from '../models/user.js';
+import { secret } from '..config/environment.js';
+
+const secureRoute = async (req, res, next) => {
+  try {
+    const authToken = req.headers.authorization;
+
+    if (!authToken || !authToken.startsWith('Bearer')) {
+      return res.status(401).send({ message: 'Unauthorized!' });
+    }
+
+    const token = authToken.replace('Bearer ', '');
+
+    jwt.verify(token, secret, async (err, data) => {
+      if (err) {
+        return res.status(401).send({ message: 'Unauthorized' });
+      }
+
+      const user = await User.findById(data.userId);
+
+      if (!user) {
+        return res.status(401).send({ message: 'Unauthorized' });
+      }
+
+      req.currentUser = user;
+
+      next();
+    });
+  } catch (error) {
+    return res.status(401).send({ message: 'Unathorized' });
+  }
+};
+
+export default {
+  secureRoute,
+};
