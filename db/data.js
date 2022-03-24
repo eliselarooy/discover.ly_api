fe78ab52c7bb8594cd5128bcf030e8ae349e3518
Elise La Rooy <eliselarooy@gmail.com>
2022-03-24 11:41:23 +0000

    added initial seed data

---
+++ db/data.js
@@ -0,0 +1,19 @@
+export const users = [
+  {
+    username: 'user1',
+    email: 'user1@email.com',
+    password: 'password!1',
+  },
+];
+
+export const spots = [
+  {
+    title: 'Kayaking the River Ouse from Barcombe Mills',
+    description:
+      'A carpark and footpaths provide access to the river Ouse at Barcombe Mills. Heads up - there is a height barrier! Paddle north and you reach the Anchor Inn, a great pub along the river which also offers boat hire. The area is popular with walkers and for picnics on sunny days.',
+    location: 'Barcombe Mills, East Sussex',
+    activity: 'Watersports',
+    img: 'https://upload.wikimedia.org/wikipedia/commons/7/71/Waterway_Junctions%2C_Barcombe_Mills_-_geograph.org.uk_-_1413834.jpg',
+    createdBy: 'user1._id',
+  },
+];
