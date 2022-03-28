053badb49041a248dd2715e1defa84c67b96e372
Elise La Rooy <eliselarooy@gmail.com>
2022-03-28 17:40:34 +0100

    updated seed file

--- db/data.js
+++ db/data.js
@@ -5,11 +5,65 @@ export const usersData = [
     email: 'user1@email.com',
     password: 'password!1',
   },
+  {
+    username: 'user2',
+    profilePicture: 'src/assets/images/preset.jpg',
+    email: 'user2@email.com',
+    password: 'password!1',
+  },
+  {
+    username: 'user3',
+    profilePicture: 'src/assets/images/preset.jpg',
+    email: 'user3@email.com',
+    password: 'password!1',
+  },
+  {
+    username: 'user4',
+    profilePicture: 'src/assets/images/preset.jpg',
+    email: 'user4@email.com',
+    password: 'password!1',
+  },
 ];
 
 export const spotsData = [
   {
     title: 'Kayaking the River Ouse from Barcombe Mills',
+    description:
+      'A carpark and footpaths provide access to the river Ouse at Barcombe Mills. Heads up - there is a height barrier! Paddle north and you reach the Anchor Inn, a great pub along the river which also offers boat hire. The area is popular with walkers and for picnics on sunny days.',
+    location: 'Barcombe Mills, East Sussex',
+    activity: 'Watersports',
+    img: 'https://upload.wikimedia.org/wikipedia/commons/7/71/Waterway_Junctions%2C_Barcombe_Mills_-_geograph.org.uk_-_1413834.jpg',
+    createdBy: '',
+  },
+  {
+    title: '#2 Kayaking the River Ouse from Barcombe Mills',
+    description:
+      'A carpark and footpaths provide access to the river Ouse at Barcombe Mills. Heads up - there is a height barrier! Paddle north and you reach the Anchor Inn, a great pub along the river which also offers boat hire. The area is popular with walkers and for picnics on sunny days.',
+    location: 'Barcombe Mills, East Sussex',
+    activity: 'Watersports',
+    img: 'https://upload.wikimedia.org/wikipedia/commons/7/71/Waterway_Junctions%2C_Barcombe_Mills_-_geograph.org.uk_-_1413834.jpg',
+    createdBy: '',
+  },
+  {
+    title: '#3 Kayaking the River Ouse from Barcombe Mills',
+    description:
+      'A carpark and footpaths provide access to the river Ouse at Barcombe Mills. Heads up - there is a height barrier! Paddle north and you reach the Anchor Inn, a great pub along the river which also offers boat hire. The area is popular with walkers and for picnics on sunny days.',
+    location: 'Barcombe Mills, East Sussex',
+    activity: 'Watersports',
+    img: 'https://upload.wikimedia.org/wikipedia/commons/7/71/Waterway_Junctions%2C_Barcombe_Mills_-_geograph.org.uk_-_1413834.jpg',
+    createdBy: '',
+  },
+  {
+    title: '#4 Kayaking the River Ouse from Barcombe Mills',
+    description:
+      'A carpark and footpaths provide access to the river Ouse at Barcombe Mills. Heads up - there is a height barrier! Paddle north and you reach the Anchor Inn, a great pub along the river which also offers boat hire. The area is popular with walkers and for picnics on sunny days.',
+    location: 'Barcombe Mills, East Sussex',
+    activity: 'Watersports',
+    img: 'https://upload.wikimedia.org/wikipedia/commons/7/71/Waterway_Junctions%2C_Barcombe_Mills_-_geograph.org.uk_-_1413834.jpg',
+    createdBy: '',
+  },
+  {
+    title: '#5 Kayaking the River Ouse from Barcombe Mills',
     description:
       'A carpark and footpaths provide access to the river Ouse at Barcombe Mills. Heads up - there is a height barrier! Paddle north and you reach the Anchor Inn, a great pub along the river which also offers boat hire. The area is popular with walkers and for picnics on sunny days.',
     location: 'Barcombe Mills, East Sussex',
