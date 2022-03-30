ef2d95acc7f2fc8f125971bb7b1f2fd91a98d04d
Tom Riley <tom.riley@outlook.com>
2022-03-30 11:03:19 +0100

    updated seed data with lat and lng

--- db/data.js
+++ db/data.js
@@ -31,6 +31,9 @@ export const spotsData = [
     description:
       'A carpark and footpaths provide access to the river Ouse at Barcombe Mills. Heads up - there is a height barrier! Paddle north and you reach the Anchor Inn, a great pub along the river which also offers boat hire. The area is popular with walkers and for picnics on sunny days.',
     location: 'Barcombe Mills, East Sussex',
+    lat: 50.9159,
+    lng: 0.0344,
+
     activity: 'Watersports',
     img: 'https://upload.wikimedia.org/wikipedia/commons/7/71/Waterway_Junctions%2C_Barcombe_Mills_-_geograph.org.uk_-_1413834.jpg',
     createdBy: '',
