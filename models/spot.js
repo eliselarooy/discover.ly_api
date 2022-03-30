680194a36975ccf6a20b1a1f608ab685cf3eeeee
Ashley Gyngell <ashleygyngell@gmail.com>
2022-03-30 09:54:31 +0100

    update

--- models/spot.js
+++ models/spot.js
@@ -13,7 +13,7 @@ const spotSchema = new mongoose.Schema(
   {
     title: { type: String, required: true },
     description: { type: String, required: true },
-    location: { type: String, required: true },
+    location: { type: String, required: false },
     activity: {
       type: String,
       enum: ['Running', 'Walking', 'Cycling', 'Swimming', 'Watersports'],
