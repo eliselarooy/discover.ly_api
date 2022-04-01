9d145cfbfd18dd35cfb14e061a6e18cd8e738c58
Elise La Rooy <eliselarooy@gmail.com>
2022-04-01 09:07:29 +0100

    improved search functionality

--- controllers/search.js
+++ controllers/search.js
@@ -3,11 +3,15 @@ import Spot from '../models/spot.js';
 const searchSpots = async (req, res, next) => {
   const searchText = req.query.text;
   searchText.replace(' ', '%20');
-  const regex = new RegExp(searchText, 'i'); // i for case insensitive
+  // const regex = new RegExp(searchText, 'i'); // i for case insensitive
 
   try {
-    const results = await Spot.find({ title: { $regex: regex } });
-
+    // const results = await Spot.find({ title: { $regex: regex } });
+    if (searchText === '') {
+      const results = await Spot.find({});
+      res.status(200).json(results);
+    }
+    const results = await Spot.find({ $text: { $search: searchText } });
     res.status(200).json(results);
   } catch (err) {
     next(err);
